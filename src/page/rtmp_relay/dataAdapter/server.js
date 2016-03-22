9c3025ac826da4ba07557daa45ea15c39f22e887
Sam <Sam@MacBook-Pro.local>
2016-03-22 10:30:46 +0800

    Fixed 解决rtmp转发监控bug

--- src/page/rtmp_relay/dataAdapter/server.js
+++ src/page/rtmp_relay/dataAdapter/server.js
@@ -44,90 +44,109 @@ module.exports = {
 			"210.76.58.5",
 			"61.240.135.204"
 		];
-		var data = {
-			nodes: [],
-			links: []
-		};
-		var nodes = [];
-		if (!servers.length) return data;
-		servers.forEach(function(s, key) {
-			var d = {
-				net: net2ip.indexOf(s.ip) >= 0 ? 1 : 0,
-				ip: s.ip,
-				ip_city: s.ip_city,
-				//name: s.ip_city + s.ip,
-				key: key,
-				ip_isp: s.ip_isp,
-				download_bandwidth: s.download_bandwidth,
-				download_connections: s.download_connections,
-				bandwidth: s.bandwidth,
-				connections: s.connections,
-				report_at: s.report_at,
-				report_svr: s.report_at,
-				dst_relay_servers: s.dst_relay_servers,
-			}
-			d.bandwidth = d3.sum(s.dst_relay_servers, function(d) {
-				return d.bandwidth;
-			});
-			d.value = Math.max(s.download_bandwidth, d.bandwidth);
-			nodes.push(d);
-			//链接颜色
-			let send_queue;
-			s.dst_relay_servers.forEach(function(r) {
-				if (r.ip == "0.0.0.0") return;
-				send_queue = -1
-				if (s.relay_stat) {
-					for (var i = 0; i < s.relay_stat.length; i++) {
-						if (s.relay_stat[i]['address'] == r.ip) {
-							send_queue = s.relay_stat[i]['ave_send_queue']
-						}
-					}
-				}
-				// console.log(send_queue)
-				data.links.push({
-					source: d.ip,
-					target: r.ip,
-					value: r.bandwidth,
-					conns: r.conns,
-					send_queue: send_queue,
-					band: r.bandwidth
-				});
-			})
-		});
-		data.links.forEach(function(link) {
-			var node;
-			node = nodes.filter(function(d) {
-				return d.ip == link.source;
-			});
-			//console.debug(node)
-			link.source = node && node.length ? node[0] : null;
-			node = nodes.filter(function(d) {
-				return d.ip == link.target;
-			});
-			link.target = node && node.length ? node[0] : null;
-		});
 		
-		//  排除无任何转发的服务器
-		nodes.forEach(function(node) {
-			var hasNode = false;
-			hasNode = !!(data.links.filter(function(link) {
-				if (!link.source || !link.target) return false;
-				return (link.source.ip == node.ip || link.target.ip == node.ip) ?
-					true : false;
-			}).length);
-			if (hasNode) {
-				data.nodes.push(node);
-			} else {
-				//console.debug('排除无任何转发的服务器:',node)
-			}
+		var data = {
+            nodes: [],
+            links: []
+        };
+        var nodes = [];
+        if (!servers.length) return data;
+        servers.forEach(function(s) {
+            var d = {
+                net: net2ip.indexOf( s.ip )>=0?1:0,
+                ip: s.ip,
+                ip_city: s.ip_city,
+                name: s.ip_city + s.ip,
+                ip_isp: s.ip_isp,
+                download_bandwidth: s.download_bandwidth,
+                download_connections: s.download_connections,
+                bandwidth: s.bandwidth,
+                connections: s.connections,
+                report_at: s.report_at,
+                report_svr: s.report_at,
+                dst_relay_servers: s.dst_relay_servers
+                // relay_stat: $.extend(true, {}, s.relay_stat)
+                    //value: Math.max(s.download_bandwidth,s.bandwidth)
+            }
+            d.bandwidth = d3.sum(s.dst_relay_servers, function(d) {
+                return d.bandwidth;
+            });
+            d.value = Math.max(s.download_bandwidth, d.bandwidth);
+            nodes.push(d);
+            //if (!s.dst_relay_servers.length) return;
+            //链接颜色
+            s.dst_relay_servers.forEach(function(r) {
+                if (r.ip == "0.0.0.0") return;
+                var send_queue = -1
+                if (s.relay_stat){
 
-		});
-		data.nodes.sort(function(a, b) {
-			if (a.net > b.net) return 1;
-			if (a.net < b.net) return -1;
-			if (a.ip == b.ip) return 0;
-			return a.ip > b.ip ? 1 : -1;
-		});
-		return data;
+                    for(var i=0;i<s.relay_stat.length;i++){
+                        if(s.relay_stat[i]['address'] == r.ip){
+                            send_queue = s.relay_stat[i]['ave_send_queue']
+                        }
+                    }
+                }
+                // console.log(send_queue)
+                data.links.push({
+                    source: d.ip,
+                    target: r.ip,
+                    value: r.bandwidth,
+                    conns: r.conns,
+                    send_queue:send_queue,
+                    band: r.bandwidth
+                });
+            })
+        });
+        data.links.forEach(function(link) {
+            var node;
+            node = nodes.filter(function(d) {
+                return d.ip == link.source;
+            });
+            link.source = node && node.length ? node[0] : null;
+            node = nodes.filter(function(d) {
+                return d.ip == link.target;
+            });
+            link.target = node && node.length ? node[0] : null;
+        });
+        for(var i=data.links.length-1;i>=0;i--){
+            var tmpLink = data.links[i];
+            if(!tmpLink.target || !tmpLink.source)data.links.splice(i,1);
+        }
+        data.links.sort(function(a, b) {
+            if(!a.source || !a.target) return 0;
+            if(!a.source || !a.target) return 0;
+            var sameCity = (a.source.ip == b.source.ip) ? 0 : (data.links.indexOf(a) > data.links.indexOf(b) ?
+                1 : -1);
+            if (sameCity !== 0) return sameCity;
+            sameCity = (a.target.ip == b.target.ip) ? 0 : (a.target.ip > b.target.ip ? 1 :
+                -1);
+            return sameCity;
+        });
+		//  排除无任何转发的服务器
+        nodes.forEach(function(node) {
+            var hasNode = false;
+            hasNode = !!(data.links.filter(function(link) {
+                if(!link.source || !link.target)return false;
+                return (link.source.ip == node.ip || link.target.ip == node.ip) ?
+                    true : false;
+            }).length);
+            if (hasNode) {
+                data.nodes.push(node);
+            }
+            else{
+                //console.log('droped node:',node)
+                //data.nodes.push(node);
+            }
+
+        });
+        data.nodes.sort(function(a,b){
+          
+           if(a.net > b.net) return 1;
+           if(a.net < b.net) return -1;
+          if(a.ip==b.ip)return 0;
+          return a.ip > b.ip ? 1 : -1;
+        });
+        return data;
+
 	},
 }
