1e1f35f2b53d42316003d182503185e6e116f909
Sam <Sam@MacBook-Pro.local>
2016-05-16 13:38:32 +0800

    Add 新版RTMP转发监控

--- src/index.jsx
+++ src/index.jsx
@@ -1,18 +1,17 @@
 import React from 'react'
 import { render } from 'react-dom'
 import { Provider } from 'react-redux'
-import { Router, Route, IndexRoute } from 'react-router'
-import createBrowserHistory from 'history/lib/createBrowserHistory'  
+import { Router, Route, IndexRoute,browserHistory } from 'react-router'
+//公共方法||类载入，用window对象访问
+import global from 'r2/global'
 import configureStore from './page/store'
 import { rootRoute } from './page/Route'
 
-const history = createBrowserHistory();
-const store = configureStore({ },history);
+
+const store = configureStore({ },browserHistory);
 render(
 	<Provider store={store}>
-		<div>
-			<Router history={history} routes={rootRoute} />
-		</div>
+		<Router history={browserHistory} routes={rootRoute} />
 	</Provider>,	
 	document.getElementById('app_container')
 )
@@ -25,3 +24,4 @@ render(
         }
     });
 }*/
+
--- src/page/rtmp_relay/dataAdapter/server.js
+++ src/page/rtmp_relay/dataAdapter/server.js
@@ -1,154 +1,86 @@
+var g_servers;
+var g_nodes;
 module.exports = {
-    transJsonToServers(jsonDatas) {
-		let servers = [];
-		let serverLastReport = [];
-		var serverList = d3.nest().key(function(d) {
-			return d.ip;
-		}).sortKeys(d3.ascending).sortValues(function(a, b) {
-			var result = (a.report_at < b.report_at) ? 1 : (a.report_at == b.report_at ? 0 :
-				-1);
-			return result;
-		});
-
-		var allIpList = serverList.entries(jsonDatas);
-		allIpList.forEach(function(d) {
-			var srv = d.values[0];
-			servers.push(d.values[0]);
-			if (!serverLastReport[d.key]) {
-				serverLastReport[d.key] = d.values[0].report_at;
-			} else {
-				if (d.values[0].report_at > serverLastReport[d.key]) {
-					serverLastReport[d.key] = d.values[0].report_at;
-				}
+	/**
+	 * 获取不同类别的server,又有的链接正常node server，无连接server，链接出错server
+	 */
+    getSortServers(jsonDatas) {
+		var servers = {
+			node: [],
+			noLink: [],
+			error: [],//放置错误的send_relays
+		};
+		//console.debug(jsonDatas)
+		jsonDatas.data.forEach((v,k)=>{
+			var flag1 = !v.recv_relays || v.recv_relays && !v.recv_relays[0];
+			var flag2 = !v.send_relays || v.send_relays && !v.send_relays[0];
+			if(flag1 && flag2){
+				servers.noLink.push(v);
+			}else{
+				servers.node.push(v);
 			}
-		});
-		return servers;
-		//console.debug(serverLastReport)
+		})
+		//console.debug(servers.node.length)
+		//console.debug(JSON.stringify(servers.noLink,null,2))
+		g_servers = servers;
+		return servers
 	},
-	makeSankeyData(servers){
-		var net2ip = [
-			"120.24.240.208",
-			"123.56.155.168",
-			"120.26.227.224",
-
-			"60.12.69.106",
-			"122.226.181.104",
-			"122.226.181.124",
-
-			"221.203.168.163",
-			"221.204.171.5",
-			"222.174.239.69",
-			"117.34.47.32",
-			"175.6.18.161",
-			"58.220.22.72",
-			"210.76.58.5",
-			"61.240.135.204"
-		];
-		
-		var data = {
-            nodes: [],
-            links: []
-        };
-        var nodes = [];
-        if (!servers.length) return data;
-        servers.forEach(function(s) {
-            var d = {
-                net: net2ip.indexOf( s.ip )>=0?1:0,
-                ip: s.ip,
-                ip_city: s.ip_city,
-                name: s.ip_city + s.ip,
-                ip_isp: s.ip_isp,
-                download_bandwidth: s.download_bandwidth,
-                download_connections: s.download_connections,
-                bandwidth: s.bandwidth,
-                connections: s.connections,
-                report_at: s.report_at,
-                report_svr: s.report_at,
-                dst_relay_servers: s.dst_relay_servers
-                // relay_stat: $.extend(true, {}, s.relay_stat)
-                    //value: Math.max(s.download_bandwidth,s.bandwidth)
-            }
-            d.bandwidth = d3.sum(s.dst_relay_servers, function(d) {
-                return d.bandwidth;
-            });
-            d.value = Math.max(s.download_bandwidth, d.bandwidth);
-            nodes.push(d);
-            //if (!s.dst_relay_servers.length) return;
-            //链接颜色
-            s.dst_relay_servers.forEach(function(r) {
-                if (r.ip == "0.0.0.0") return;
-                var send_queue = -1
-                if (s.relay_stat){
-
-                    for(var i=0;i<s.relay_stat.length;i++){
-                        if(s.relay_stat[i]['address'] == r.ip){
-                            send_queue = s.relay_stat[i]['ave_send_queue']
-                        }
-                    }
-                }
-				//if(send_queue == -1){
-					 //console.debug(s.relay_stat)
+	getNodeData(){
+		g_nodes = [];
+		g_servers.node.forEach(function(v,k){
+			g_nodes.push(v)
+		})
+		return g_nodes;	
+	},
+	getLinkData(){
+		var servers = g_servers.node;
+		var links = [];
+		var nodes = servers;
+		servers.forEach((v,k)=>{
+			if(v.send_relays && v.send_relays[0]){
+				//if(v.address == "tz-relay7:1935"){
+					//console.debug(v)
 				//}
-                data.links.push({
-                    source: d.ip,
-                    target: r.ip,
-                    value: r.bandwidth,
-                    conns: r.conns,
-                    send_queue:send_queue,
-                    band: r.bandwidth
-                });
-            })
-        });
-        data.links.forEach(function(link) {
-            var node;
-            node = nodes.filter(function(d) {
-                return d.ip == link.source;
+				v.send_relays.forEach((v2,k2)=>{
+					//console.debug(Math.max(v.bw_in,v.bw_out))
+					links.push({
+						source: v.address,
+						target: v2.address,
+						is_lan: v2.is_lan,
+						//value: Math.max(v.bw_in,v.bw_out),
+					})
+				})
+			}
+		})
+		links.forEach(function(link) {
+            var node,s_1,t_1;
+            node = nodes.filter(function(d,k) {
+                return d.address == link.source;
             });
             link.source = node && node.length ? node[0] : null;
-            node = nodes.filter(function(d) {
-                return d.ip == link.target;
+            node = nodes.filter(function(d,k) {
+                return d.address == link.target;
             });
+			//console.debug(node)
             link.target = node && node.length ? node[0] : null;
         });
-        for(var i=data.links.length-1;i>=0;i--){
-            var tmpLink = data.links[i];
-            if(!tmpLink.target || !tmpLink.source)data.links.splice(i,1);
-        }
-        data.links.sort(function(a, b) {
-            if(!a.source || !a.target) return 0;
-            if(!a.source || !a.target) return 0;
-            var sameCity = (a.source.ip == b.source.ip) ? 0 : (data.links.indexOf(a) > data.links.indexOf(b) ?
-                1 : -1);
-            if (sameCity !== 0) return sameCity;
-            sameCity = (a.target.ip == b.target.ip) ? 0 : (a.target.ip > b.target.ip ? 1 :
-                -1);
-            return sameCity;
-        });
-		//  排除无任何转发的服务器
-        nodes.forEach(function(node) {
-            var hasNode = false;
-            hasNode = !!(data.links.filter(function(link) {
-                if(!link.source || !link.target)return false;
-                return (link.source.ip == node.ip || link.target.ip == node.ip) ?
-                    true : false;
-            }).length);
-            if (hasNode) {
-                data.nodes.push(node);
-            }
-            else{
-                //console.log('droped node:',node)
-                //data.nodes.push(node);
-            }
-
-        });
-        data.nodes.sort(function(a,b){
-          
-           if(a.net > b.net) return 1;
-           if(a.net < b.net) return -1;
-          if(a.ip==b.ip)return 0;
-          return a.ip > b.ip ? 1 : -1;
-        });
-        return data;
-
+		links = links.filter((v,k)=>{
+			return !!v.source && !!v.target; 
+		})
+		links.forEach(m=>{
+			//console.debug(nodes.indexOf(m.target))
+			m.target.recv_relays.forEach((v,k)=>{
+				m.value = v.bw_in;
+				m.bw_in = v.bw_in;
+				//console.debug(v.bw_in)
+			})
+			m.source = nodes.indexOf(m.source)
+			m.target = nodes.indexOf(m.target)
+		})
+		links = links.filter((v,k)=>{
+			return v.value; 
+		})
+		//console.debug(links)
+		return links;
 	},
 }
