9174eb232df9d4f09989e15288746fc036ac951c
dearYan <673822488@qq.com>
2016-06-21 11:10:45 +0800

    企业认证修改

--- src/page/enterprise_manager_authenticate/components/Dailog.js
+++ src/page/enterprise_manager_authenticate/components/Dailog.js
@@ -26,7 +26,6 @@ let ViewImg = React.createClass({
     
     componentWillReceiveProps(nextProps){
         const { visible } = nextProps;
-
     },
 
     componentDidUpdate(){
@@ -54,7 +53,9 @@ export const Dailog = React.createClass({
             isFinished : false,
             visible : false,
             width : 80,
-            height : 50
+            height : 50,
+            random : 0,
+            isSuccess : "success"
         }
     },
 
@@ -67,12 +68,23 @@ export const Dailog = React.createClass({
             });
             if(dailogData["param"]["url"] != undefined && !this.state.isFinished){
                 var img = new Image();
-                img.src = dailogData["param"]["url"];
+                var random = new Date().getTime()+generateMixed(6);
+                img.src = dailogData["param"]["url"]+"?random="+random;
+                img.onerror = function(){
+                    this.setState({
+                        isSuccess : "error",
+                        visible : true,
+                        isFinished : true,
+                        random : random
+                    });
+                }.bind(this);
                 img.onload = function(){
                     this.setState({
                         isFinished : true,
                         width : img.width,
-                        height : img.height
+                        height : img.height,
+                        isSuccess : "success",
+                        random : random
                     });
                 }.bind(this);
             }
@@ -82,7 +94,11 @@ export const Dailog = React.createClass({
     handleCancel(){
         const { dispatch } = this.props
     	this.setState({
-    		visible: false
+    		visible: false,
+            isFinished: false,
+            width : 80,
+            height : 50,
+            random : 0,
     	});
         dispatch(authenticateDailog({
             visible: false
@@ -92,7 +108,8 @@ export const Dailog = React.createClass({
     render(){
         const _this = this;
     	const { dailogData } = this.props;
-        var content = '';
+        var content = '',
+            htl = '';
         
         if(!isEmptyObj(dailogData) && !isEmptyObj(dailogData["param"]["visible"])){
             return false;
@@ -100,15 +117,21 @@ export const Dailog = React.createClass({
 
         if(this.state.visible && !this.state.isFinished){
             return false;
-           // return <ViewImg { ...this.props } visible = { this.state.isFinished }/>
         }else if(this.state.visible && this.state.isFinished){
+
+            if(this.state.isSuccess === "success"){
+                htl = <img src = { dailogData["param"]["url"]+"?random="+this.state.random }/>
+            }else{
+                htl = <div>图片加载错误</div>
+            }
+
             return <Modal title="查看执照" className = "enterprise_manager_authenticate_daliog" 
                 visible={ _this.state.visible }
                 width = { _this.state.width + 60 }
                 height = { _this.state.height + 32 }
-                onOk={ _this.handleOk } 
-                onCancel={ _this.handleCancel }>
-                <img src={ dailogData["param"]["url"]+"?random="+new Date().getTime()+generateMixed(6) }/>
+                onOk = { _this.handleOk } 
+                onCancel = { _this.handleCancel }>
+                { htl }
             </Modal>
         }else{
             return false;
