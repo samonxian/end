de565ffd1086ad435928ab4f7350c74a20880a2b
Sam <Sam@Sam-MacBook.local>
2016-08-25 14:35:04 +0800

    Modify 替换日志

--- src/page/user_log/rtmp_open_time/data.jsx
+++ src/page/user_log/rtmp_open_time/data.jsx
@@ -8,8 +8,14 @@ let column_dataIndexs = [
 	'area',
 	'session',
 	'cid',
-	'data_open_time',
-	'open_time',
+	'relay_server',
+	'DNSCosttime',
+	'connect0time',
+	'connect1time',
+	'connect2time',
+	'connect3time',
+	'connect4time',
+	'connectType',
 	'reserved',
 ]
 export let columns = [
@@ -33,10 +39,44 @@ export let columns = [
 		title: '摄像头id',
 	},
 	{
-		title: '收到第一帧数据的总时间',
+		title: '3个转发服务器',
+		render(text,columns){
+			return (
+				<span>
+					{
+						text.map((v,k)=>{
+							return (
+								<span key={k}>
+									{v.ip + ":" + v.port} 
+									<br />
+								</span>
+							)
+						})
+					}
+				</span>
+			)
+		}
+	},
+	{
+		title: 'DNS解析耗时',
+	},
+	{
+		title: 'TCP连接耗时',
+	},
+	{
+		title: 'RTMP握手耗时',
+	},
+	{
+		title: 'RTMP连接耗时',
 	},
 	{
 		title: '收到第一个包的总时间',
+	},
+	{
+		title: '收到第一帧数据的总时间',
+	},
+	{
+		title: '客户端类型',
 	},
 	{
 		title: '保留字段',
@@ -55,6 +95,15 @@ export function logData(user_log){
 			temp_data.area = temp_data.public_ip_country + "/" + temp_data.public_ip_prov 
 									+ "/" + temp_data.public_ip_city 
 									+ "/" + temp_data.public_ip_isp
+			switch(temp_data.connectType){
+				case 1:
+					temp_data.connectType = "拉流端";
+					break;	
+				case 2:
+					temp_data.connectType = "推流端";
+					break;	
+			}
+			
 		})
 	}
 	return re;
